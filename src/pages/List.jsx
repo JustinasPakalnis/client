5c2ccb31436c5904b3a27498014c0f7385e27e2a
JustinasPakalnis <justinas.p@inbox.lt>
2024-09-22 15:19:49 +0300

    yes

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -15,6 +15,7 @@ const List = () => {
     };
     fetchAllItems();
   }, []);
+  console.log(items);
 
   const handleDelete = async (id) => {
     try {
@@ -26,37 +27,53 @@ const List = () => {
   };
 
   return (
-    <div className="listContainer">
-      <ul className="list">
-        <div className="title">
-          <p>Item</p>
-          <p>Owner</p>
-          <p>Location</p>
-          <p>Value</p>
-        </div>
-
-        {items.map((items, index) => (
-          <li
-            className={index % 2 === 0 ? "listItem" : "listItem2"}
-            key={items.id}
-          >
-            <p>{items.item}</p>
-            <p>{items.owner}</p>
-            <p>{items.location}</p>
-            <p>{items.value.toFixed(2)} Eur</p>
-            <button className="button" onClick={() => handleDelete(items.id)}>
-              Delete
-            </button>
-            <button className="button">
-              <Link to={`/update/${items.id}`}>Update</Link>
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button>
+    <>
+      <button className="addItem">
         <Link to="/add">Add new item</Link>
       </button>
-    </div>
+      <div className="listContainer">
+        <hr />
+        <div className="title">
+          <div className="summary">
+            <p>Total warehouse amount: </p>
+            <p>{items.length}</p>
+          </div>
+          <div className="summary">
+            <p>Total warehouse value: </p>
+            <p>
+              {items.reduce((acc, currentItem) => acc + currentItem.value, 0)}{" "}
+              Eur
+            </p>
+          </div>
+        </div>
+        <ul className="list">
+          <div className="title">
+            <p>Item</p>
+            <p>Owner</p>
+            <p>Location</p>
+            <p>Value</p>
+          </div>
+
+          {items.map((items, index) => (
+            <li
+              className={index % 2 === 0 ? "listItem" : "listItem2"}
+              key={items.id}
+            >
+              <p>{items.item}</p>
+              <p>{items.owner}</p>
+              <p>{items.location}</p>
+              <p>{items.value.toFixed(2)} Eur</p>
+              <button className="button" onClick={() => handleDelete(items.id)}>
+                Delete
+              </button>
+              <button className="button">
+                <Link to={`/update/${items.id}`}>Update</Link>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
